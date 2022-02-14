cf4dfe42ef09751e18dd902cfc2c50e5bc8aca4b
raman2021 <A00244354@mycambrian.ca>
2022-02-14 01:24:51 -0500

    adding interface and page for the list of jobs with adding some job details

---
+++ JobManager/Services/JobDataStoreLocalJson.cs
@@ -0,0 +1,44 @@
+﻿using JobManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobManager.Services
+{
+    class JobDataStoreLocalJson : IJobDataStore<Job>
+    {
+        public Task AddJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Job> GetJob(int jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Job>> GetJobs()
+        {
+            var jobs = new List<Job>()
+           {
+               new Job {Id = 1, Name = "Job A Local Json File", Description = "This is job a." },
+               new Job {Id = 2, Name = "Job B Local Json File", Description = "This is job b." },
+               new Job {Id = 3, Name = "Job C Local Json File", Description = "This is job c." },
+               new Job {Id = 4, Name = "Job D Local Json File", Description = "This is job d." }
+           };
+            return jobs;
+            //throw new NotImplementedException();
+        }
+
+        public Task UpdateJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
