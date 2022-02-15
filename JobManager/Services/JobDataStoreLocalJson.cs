361ded33a9fbebd4854f3958a68c4cf70536dda2
raman2021 <A00244354@mycambrian.ca>
2022-02-15 00:06:35 -0500

    works on datastore with creating read and write file so that data can be updated,deleted or edited by using their id.

--- JobManager/Services/JobDataStoreLocalJson.cs
+++ JobManager/Services/JobDataStoreLocalJson.cs
@@ -1,6 +1,8 @@
 ﻿using JobManager.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,22 +10,85 @@ namespace JobManager.Services
 {
     class JobDataStoreLocalJson : IJobDataStore<Job>
     {
+        public static string FilePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, "Jobs.json");
+            }
+        }
         public async Task AddJob(Job job)
         {
-            throw new NotImplementedException();
+            var jobs = ReadFile();
+            jobs.Add(job);
+            WriteFile(jobs);
+            //throw new NotImplementedException();
         }
 
         public async Task DeleteJob(Job job)
         {
-            throw new NotImplementedException();
+            var jobs = ReadFile();
+            var remove = jobs.Find(p => p.Id == job.Id);
+            jobs.Remove(remove);
+            //throw new NotImplementedException();
         }
 
         public async Task<Job> GetJob(int jobId)
         {
-            throw new NotImplementedException();
+            var jobs = ReadFile();
+            var job = jobs.Find(p => p.Id == jobId);
+            return job;
+            //throw new NotImplementedException();
         }
 
         public async Task<IEnumerable<Job>> GetJobs()
+        {
+            var jobs = ReadFile();
+            return jobs;
+          /*  var jobs = new List<Job>()
+           {
+               new Job {Id = 1, Name = "Job A Local Json File", Description = "This is job a." },
+               new Job {Id = 2, Name = "Job B Local Json File", Description = "This is job b." },
+               new Job {Id = 3, Name = "Job C Local Json File", Description = "This is job c." },
+               new Job {Id = 4, Name = "Job D Local Json File", Description = "This is job d." }
+           };
+            return jobs; */
+            //throw new NotImplementedException();
+        }
+
+        public async Task UpdateJob(Job job)
+        {
+            var jobs = ReadFile();
+            jobs[jobs.FindIndex(p => p.Id == job.Id)] = job;
+            WriteFile(jobs);
+            //throw new NotImplementedException();
+        }
+
+        private void WriteFile (List<Job> jobs)
+        {
+            var jsonString = JsonConvert.SerializeObject(jobs);
+            File.WriteAllText(FilePath, jsonString);
+        }
+
+        private List<Job> ReadFile()
+        {
+            if (File.Exists(FilePath))
+            {
+                var jsonString = File.ReadAllText(FilePath);
+                var jobs = JsonConvert.DeserializeObject<List<Job>>(jsonString);
+                return jobs;
+            }
+            else
+            {
+                var jobs = GetDefaultJobs();
+                WriteFile(jobs);
+                return jobs;
+            }
+
+        }
+
+        private List<Job> GetDefaultJobs()
         {
             var jobs = new List<Job>()
            {
@@ -33,12 +98,7 @@ namespace JobManager.Services
                new Job {Id = 4, Name = "Job D Local Json File", Description = "This is job d." }
            };
             return jobs;
-            //throw new NotImplementedException();
         }
 
-        public async Task UpdateJob(Job job)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
