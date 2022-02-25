d4c66438800b8b4c10875ef578f1897e065d50f5
raman2021 <A00244354@mycambrian.ca>
2022-02-24 20:48:20 -0500

    Add new json service file for blob

---
+++ JobManager/Services/JobDataStoreBlobStorageJson.cs
@@ -0,0 +1,40 @@
+﻿using JobManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobManager.Services
+{
+    class JobDataStoreBlobStorageJson : IJobDataStore<Job>
+    {
+        public async Task AddJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+       /* public Task DeleteJob(Job job)
+        {
+            throw new NotImplementedException();
+        } 
+
+        public async Task<Job> GetJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+       */
+        public async Task<Job> GetJob(int jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Job>> GetJobs()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task UpdateJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
