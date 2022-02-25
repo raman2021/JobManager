8c4b6539aafe2ccfe0f534e98c3f0f4b814d2455
raman2021 <A00244354@mycambrian.ca>
2022-02-24 20:20:18 -0500

    binding content page to viewmodel

---
+++ JobManager/ViewModels/JobDetailViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace JobManager.ViewModels
+{
+    [
+
+        QueryProperty(nameof(JobId), nameof(JobId))]
+    //public class JobDetailViewModel : JobManagerBase
+    //public
+    class JobDetailViewModel : JobManagerBase
+    {
+        private int jobId;
+
+        public int JobId
+        {
+
+            get
+            {
+                return jobId;
+            }
+            set
+            {
+                jobId = value;
+                LoadJob(value);
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        private string description;
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+        public async void LoadJob(int jobId)
+        {
+
+            var job = await JobDataStore.GetJob(jobId);
+            Name = job.Name;
+            Description = job.Description;
+        }
+
+
+
+    }
+}
