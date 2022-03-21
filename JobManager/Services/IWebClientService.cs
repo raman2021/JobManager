b925faae0a3637040088933d93a9bc3046b96503
raman2021 <A00244354@mycambrian.ca>
2022-03-21 19:20:07 -0400

    solving some errors and adding post async

--- JobManager/Services/IWebClientService.cs
+++ JobManager/Services/IWebClientService.cs
@@ -8,7 +8,8 @@ namespace JobManager.Services
     public interface IWebClientService
     {
         //
-        Task<string> GetString(string uri);
+        Task<string> GetAsync(string uri);
+        Task<string> PostAsync(string uri, string body, string type);
 
     }
 }
