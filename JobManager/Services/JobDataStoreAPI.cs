eec90752d0b708348d4bdededa503a6c414f7bf0
raman2021 <A00244354@mycambrian.ca>
2022-03-21 19:16:59 -0400

    geeting jobs in JobDataStoreAPI file

---
+++ JobManager/Services/JobDataStoreAPI.cs
@@ -0,0 +1,63 @@
+﻿using JobManager.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JobManager.Services
+{
+    class JobDataStoreAPI : IJobDataStore<Job>
+    {
+
+        private static string StudentNumber => "A00244354";
+        private static string API => $"https://jobmanagerdevapi.azurewebsites.net/{StudentNumber}";
+        public async Task AddJob(Job job)
+        {
+            var service = DependencyService.Get<IWebClientService>();
+
+            var jsonString = await service.PostAsync($"{API}/Jobs", JsonConvert.SerializeObject(job), "application/json");
+
+            if (jsonString != null)
+            {
+                var newJob = JsonConvert.DeserializeObject<Job>(jsonString);
+            }
+
+            //Consider returning a bool to verify if the job was created.
+        }
+
+        public Task DeleteJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Job> GetJob(int jobId)
+        {
+            var service = DependencyService.Get<IWebClientService>();
+            var jsonString = await service.GetAsync($"{API}/Jobs/{jobId}");
+
+            var job = JsonConvert.DeserializeObject<Job>(jsonString);
+
+            return job;
+        }
+
+        public async Task<IEnumerable<Job>> GetJobs()
+        {
+            var service = DependencyService.Get<IWebClientService>();
+            var jsonString = await service.GetAsync($"{API}/Jobs");
+            var jobs = JsonConvert.DeserializeObject<List<Job>>(jsonString);
+            return jobs;
+        }
+
+        public async Task UpdateJob(Job job)
+        {
+            var service = DependencyService.Get<IWebClientService>();
+
+            await service.PutAsync($"{API}/Jobs/{job.ID}", JsonConvert.SerializeObject(job), "application/json");
+
+            //Consider returning a bool to verify if the job was updated.
+        }
+    }
+}
+
